8228f0251c42ba6da427b906dbbd0bc47414c0a8
yellowcanstop <yellowcanstop@gmail.com>
2026-03-11 19:13:50 +0800

    Change file path.

--- src/lib/Uploader.svelte
+++ src/lib/Uploader.svelte
@@ -35,7 +35,7 @@
     try {
       error = ""; 
 
-      const response = await fetch(`./public/samples/${fileName}`);
+      const response = await fetch(`/samples/${fileName}`);
       if (!response.ok) throw new Error("Could not find the sample file.");
 
       const blob = await response.blob();
